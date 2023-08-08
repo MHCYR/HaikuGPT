03198333f4450884830c2887ce75bcd3d38a749c
Mohacyr Rojas <mohacyr.rojas.contractor@bbva.com>
2023-08-08 13:42:39 -0600

    feat: add cor

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express, { Express } from "express";
 import morgan from "morgan";
+import cors from "cors";
 
 const app: Express = express();
 app.use(morgan("dev")); // logs requests to the console
 app.use(express.json()); // creates an object from the request body
 app.use(express.urlencoded({ extended: true })); // creates an object from the request url
+app.use(cors()); // allows requests from other origins
 
 app.get("/", (_req, res) => {
   res.send("Hello Wjaslkdfjaorld!");
